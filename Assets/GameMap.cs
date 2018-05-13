33b11002613c4b5ce0851a6b1986d4b8db73d864
Douglas May <douglas.may@red-inc.us>
2018-05-13 08:34:54 -0400

    Save Progress

---
+++ Assets/GameMap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMap : MonoBehaviour {
+
+
+
+    private bool is_initialized = false;
+    public GameObject GameNodePrefab;
+
+
+    public void InitializeMap(int height, int width)
+    {
+
+
+
+        GameObject bot_left_corner = Instantiate(GameNodePrefab);
+        bot_left_corner.GetComponent<GameNode>().Initialize(0);
+
+
+    }
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+
+    private void build_map_rec(GameObject node_obj, int id)
+    {
+        if(node_obj == null)
+        {
+            return;
+        }
+
+        GameNode node = node_obj.GetComponent<GameNode>();
+        if(node.GetID - 1  )
+    }
+}
---
+++ Assets/GameNode.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class GameNode : MonoBehaviour {
+    //OPPOSITES NEED TO BE NEXT TO EACH OTHER OTHERWISE THE 
+    //ANTI FUNCTION WILL BREAK;
+    public enum NeighborDirection { North=0, South=1, East=2, West=3 };
+    
+    private GameNode[] neighbors = new GameNode[System.Enum.GetNames(typeof(NeighborDirection)).Length];
+
+    private Color GameNode_color;
+    private int gen_id;
+    
+
+    public void Initialize(int id)
+    {
+        gen_id = id;
+    }
+
+    public int  GetID()
+    {
+        return gen_id;
+    }
+
+    public void SetNeighbor(GameNode new_neighbor, NeighborDirection direction)
+    {
+
+        neighbors[(int)direction] = new_neighbor;
+        new_neighbor.SetNeighbor(this, AntiDirection(direction));
+
+    }
+
+    public void SetNeighbors(GameNode[] new_neighbors)
+    {
+        neighbors = new_neighbors;
+    }
+
+    public GameNode[] GetNeighbors()
+    {
+        return neighbors;
+    }
+
+    private NeighborDirection AntiDirection(NeighborDirection direction)
+    {
+        int LorR = (int)direction & 1;
+        int high_bits = (int)direction & -2;
+
+        return (NeighborDirection)(~LorR ^ high_bits);
+
+    }
+    
+
+    public void SwapWithNeighbor(GameNode swap_neighbor,NeighborDirection direction)
+    {
+        GameNode[] neighbor_neighbors = swap_neighbor.GetNeighbors();
+
+        swap_neighbor.SetNeighbors(neighbors);
+        this.neighbors = neighbor_neighbors;
+        SetNeighbor(swap_neighbor, AntiDirection(direction));
+
+    }
+
+
+
+}
+
+
+
+
+
+
+
+
